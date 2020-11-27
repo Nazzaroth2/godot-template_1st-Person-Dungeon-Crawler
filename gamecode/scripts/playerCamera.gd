c559c847dde171392ee88a2e8eb5e2a1a21b5faa
Nazzaroth2 <cernyviktor@web.de>
2020-11-27 08:22:23 +0100

    complete camera movement
    though without collision checks

--- gamecode/scripts/playerCamera.gd
+++ gamecode/scripts/playerCamera.gd
@@ -1,19 +1,23 @@
 extends Camera
 
-var movement_flag: String = "stop"
+#This Script deals with the movement of the player camera. Events will trigger
+#when they collide with this node. You can adjust movement Behaviour with 
+#the exported variables, also keep the level-grid size the same as this gridSize
+#variable.
+#If you dont want to use random Encounters and rather use real Enemies that move
+#on the map, one possible adjustment ist this:
+#Dont reset the movement_flag after a movement, rather emit a signal that player
+#has moved. Then let the pc handle all enemy movement and emit a signal again.
+#On this signal we set movement_flag back to "stop" and can move the player again.
 
-var is_moving: String = "stop"
-var movementStep: float = 0
-var gridSize: int = 2
-var movementSpeed: int = 2
+
+export var gridSize: int = 2
+export var movementSpeed: int = 2
 export var rotationSpeed: float = 10
-var rotationLimit: float = 100/rotationSpeed
-var rotationStep: float = 1.570796 / rotationLimit
-var rotationState: float = 0
-var elapsed: float = 0.0
+
+var movement_flag: String = "stop"
+var movementStep: float = 0
 var target_rotation = Vector3()
-
-
 var directionDict: Dictionary = {
 	"N":[Vector3(0,0,-1)],
 	"S":[Vector3(0,0,1)],
@@ -22,13 +26,6 @@ var directionDict: Dictionary = {
 }
 
 
-
-#func _unhandled_input(event):
-#	if event.is_action_pressed("N"):
-#		move_forward()
-#	if event.is_action_released("N"):
-#		N_flag = false
-		
 func _process(delta):
 	#setting the direction flag if no prior movement is running
 	if movement_flag == "stop":
@@ -41,32 +38,30 @@ func _process(delta):
 		if Input.is_action_pressed("W"):
 			movement_flag = "W"
 		if Input.is_action_pressed("rotateLeft"):
-			movement_flag = "rotateLeft"
+			movement_flag = "rotate"
 			target_rotation = rotation_degrees + Vector3(0,90,0)
 			target_rotation.round()
 		if Input.is_action_pressed("rotateRight"):
-			movement_flag = "rotateRight"
+			movement_flag = "rotate"
 			target_rotation = rotation_degrees + Vector3(0,-90,0)
 			target_rotation.round()
 		
-		
+	#running movement function every frame until flag is reset
 	if movement_flag in ["N","S","E","W"]:
-		move(delta, movement_flag)
-	elif movement_flag != "stop":
-		rotate(delta, movement_flag)
+		move_player(delta, movement_flag)
+	elif movement_flag == "rotate":
+		rotate_player(delta)
 		
 
 
-
-
-func move(delta, moveDir):
+func move_player(delta, moveDir):
 	#moving camera one microstep towards direction
 	translate_object_local(directionDict[moveDir][0]*delta*movementSpeed)
 	#keeping track of step progress
 	movementStep += delta*movementSpeed
 	#one step is same as gridSize. When we finished one full Step we
 	#stop the step and reset the flags plus rounding towards closest grid-position
-	#we than either immediatly start next step in the same direction(continious walking)
+	#we then either immediatly start next step in the same direction(if button pressed)
 	#or we can do any other movement action
 	if movementStep >= gridSize:
 		#fix small offset from goal by rounding towards step goal
@@ -75,54 +70,16 @@ func move(delta, moveDir):
 		movement_flag = "stop"
 		movementStep = 0
 		print(global_transform.origin)
+
+
+func rotate_player(delta):
+	#rotation towards target_rotation-direction with lerp function for smooth rotation
+	rotation_degrees = Vector3(0, 
+			lerp(rotation_degrees.y, target_rotation.y, rotationSpeed * delta),0)
 	
-func rotate(delta, rotDir):
-	if rotDir == "rotateLeft":
-		rotation_degrees = Vector3(0, 
-				lerp(rotation_degrees.y, target_rotation.y, rotationSpeed * delta),0)
-		
-#		rotate_y(rotationStep * delta)
-#		rotationState += rotationStep * delta
-#
-#		print(rotationState)
-#		print(rotationStep*delta)
-
-		print(rotation_degrees)
-		print(target_rotation)
-#		print("-----")
-#		print(rotation_degrees.round())
-#		print(target_rotation.round())
-#
-		if rotation_degrees.round() == target_rotation.round():
-			movement_flag = "stop"
-			rotationState = 0
-			print("finished")
-#			set_rotation_degrees(rotation_degrees.floor())
-			set_rotation_degrees(Vector3(floor(rotation_degrees.x),
-					round(rotation_degrees.y),round(rotation_degrees.z)))
-#			print(rotation_degrees)
-	elif rotDir == "rotateRight":
-		rotation_degrees = Vector3(0, 
-				lerp(rotation_degrees.y, target_rotation.y, rotationSpeed * delta),0)
-		
-#		rotate_y(rotationStep * delta)
-#		rotationState += rotationStep * delta
-#
-#		print(rotationState)
-#		print(rotationStep*delta)
-
-		print(rotation_degrees)
-		print(target_rotation)
-#		print("-----")
-#		print(rotation_degrees.round())
-#		print(target_rotation.round())
-#
-		if rotation_degrees.round() == target_rotation.round():
-			movement_flag = "stop"
-			rotationState = 0
-			print("finished")
-#			set_rotation_degrees(rotation_degrees.floor())
-			set_rotation_degrees(Vector3(floor(rotation_degrees.x),
-					round(rotation_degrees.y),round(rotation_degrees.z)))
-#			print(rotation_degrees)
-
+	#when we reach the target close enough we stop rotation and set the rotation
+	#cleanly ourselfs
+	if rotation_degrees.round() == target_rotation.round():
+		movement_flag = "stop"
+		rotation_degrees = Vector3(round(rotation_degrees.x),
+				round(rotation_degrees.y),round(rotation_degrees.z))
